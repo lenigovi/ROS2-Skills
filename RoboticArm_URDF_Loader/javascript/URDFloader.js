fb631e2cd1bdd47df1babc1bbba289adb9054dae
lenigovi <elenaherzegovina@gmail.com>
2024-09-15 07:02:16 +0300

    urdf loader

---
+++ RoboticArm_URDF_Loader/javascript/URDFloader.js
@@ -0,0 +1,5 @@
+import * as THREE from 'three';
+import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
+import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader.js';
+import { URDFRobot, URDFJoint, URDFLink, URDFCollider, URDFVisual, URDFMimicJoint } from './URDFClasses.js';
+
